35979be35582bb2d059659282c19b9b4395e8ee1
jinWan0823 <dhksl823@gmail.com>
2023-11-06 17:38:49 +0900

    ✨ 신규유저 정보 저장

--- utils/io.js
+++ utils/io.js
@@ -1,6 +1,18 @@
+const userController = require("../Controllers/userController");
+
 module.exports = function (io) {
   io.on("connection", async (socket) => {
     console.log("client is connected", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      //유저정보 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log(socket.id, "user is disconnected");
